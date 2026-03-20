135f6c015839eeff466ac596cc87be7e772ecf5a
Buruk Kassahun <brookkasahun@gmail.com>
2026-03-20 17:24:28 +0300

    feat(app): add lambda handlers skeleton

---
+++ app/handlers/getItem.js
@@ -0,0 +1,17 @@
+exports.handler = async (event) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "getItem handler placeholder",
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Internal Server Error",
+      }),
+    };
+  }
+};
