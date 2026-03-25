1f9f5cb0e36a6ab2ccf386fd3c06813eee88523b
Buruk Kassahun <brookkasahun@gmail.com>
2026-03-25 11:02:35 +0300

    feat(s3): add S3 module and bucket

---
+++ infrastructure/terraform/modules/s3/main.tf
@@ -0,0 +1,26 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project = "MediFlow"
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
